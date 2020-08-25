acd8265e075e9eb20d5ad97b2934fde152b65cd2
admincrystal <mr.brent.hansen@gmail.com>
2020-08-24 22:28:05 -0700

    Fixed snake_case names

--- Menus/ControlsMenu.gd
+++ Menus/ControlsMenu.gd
@@ -15,13 +15,13 @@ func _ready():
 
 
 func _on_StandardControls_pressed():
-	Controls.set_controls("user://standardControls.controls")
-	Controls.change_preference("controls_file", "user://standardControls.controls")
+	Controls.set_controls("user://standard_controls.controls")
+	Controls.change_preference("controls_file", "user://standard_controls.controls")
 
 
 func _on_DvorakControls_pressed():
-	Controls.set_controls("user://dvorakControls.controls")
-	Controls.change_preference("controls_file", "user://dvorakControls.controls")
+	Controls.set_controls("user://dvorak_controls.controls")
+	Controls.change_preference("controls_file", "user://dvorak_controls.controls")
 
 func _on_BackButton_pressed():
 	Menu.visible = true
@@ -33,6 +33,6 @@ func _on_CloseMenus_pressed():
 
 
 func _on_testButton_pressed():
-	var controls_file = "user://customControls.controls"
+	var controls_file = "user://custom_controls.controls"
 	Controls.change_control("b", "c", controls_file)
 	Controls.change_preference("controls_file", controls_file)
